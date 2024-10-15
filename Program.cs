cbf046f9edfc34b148ee34c79d6ff6f71f82e5a3
Бычков <dmitriybychkov75146@gmail.com>
2024-10-15 11:18:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using practic;
+
+namespace практика_класс
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            int mmm = 1000;
+            List<first_class> list = new List<first_class>();
+            for (int i =0; i < mmm; i++)
+            {
+                first_class Obj = new first_class();
+                Obj.Pole("gzsdjkgzdshyghdfzs ");
+                Obj.Dop2El(1, i);                
+                list.Add(Obj);
+
+            }
+            //string выв = Obj.ConvertInString();
+            Console.WriteLine(list[0].ConvertInString());
+            Console.WriteLine(list[100].ConvertInString());
+            Console.WriteLine(list[999].ConvertInString());
+
+            first_class Nobj = new first_class();
+            for (int i = 0; i < mmm; i++)
+            {                
+                first_class X = list[i];                
+                string y = X.yourName();
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ first class.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practic
+{
+    internal class first_class
+    {
+        string a;
+        int b;
+        List<int> list = new List<int>();
+        public void Pole(string s)
+        {
+            a = s;
+        }
+        public void Dop2El(int x, int y)
+        {
+            list.Add(x);
+            list.Add(y);    
+        }
+        public string ConvertInString()
+        {                        
+            return a + String.Join(", ", list);
+        }
+        public string yourName()
+        {
+            return a;
+        }
+        public string yourNamelist()
+        {
+            return;
+        }
+    }
+    
+
+    
+}
